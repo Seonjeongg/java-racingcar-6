7a84da5eb9ad33ef3eaec90df535874274894ef1
Seonjeongg <sj030205@naver.com>
2024-09-26 13:34:39 +0900

    feat:전진 조건 확인 파악

--- src/main/java/racingcar/Service/CarService.java
+++ src/main/java/racingcar/Service/CarService.java
@@ -1,5 +1,26 @@
 package racingcar.Service;
 
-public class CarService {
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.Model.Car;
 
+import java.util.ArrayList;
+
+public class CarService {
+    public void getRandomValues(ArrayList<Car> cars) {
+        for(int i=0;i<cars.size();i++){
+            int n=generateRandomValue();
+            if(n>=4) moveCar();
+            else stopCar();
+        }
+    }
+
+    private int generateRandomValue(){
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+    private void moveCar(){
+
+    }
+
+    private void stopCar(){}
 }
